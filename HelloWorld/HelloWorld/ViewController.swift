c534ea1e3136f310ecb4707610462bda36e78de2
kdy <kokbael@naver.com>
2025-03-12 11:44:27 +0900

    Swift로 다시 생성

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 김동영 on 3/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("ViewController.viewDidLoad()")
+    }
+
+
+}
+
