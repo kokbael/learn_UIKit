c6e0799d1a90eee77d84b7dcb9853ea6b2401c05
kdy <kokbael@naver.com>
2025-03-12 13:48:42 +0900

    SecondViewController 추가 및 ViewController에 버튼 추가: 두 번째 화면으로 이동 기능 구현

---
+++ HelloWorld/HelloWorld/SecondViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SecondViewController.swift
+//  HelloWorld
+//
+//  Created by 김동영 on 3/12/25.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      print("2 ViewController.viewWillAppear()")
+    }
+
+    override func viewIsAppearing(_ animated: Bool) {
+      super.viewIsAppearing(animated)
+      print("3 ViewController.viewIsAppearing()")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+      super.viewDidAppear(animated)
+      print("4 ViewController.viewDidAppear()")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+      super.viewWillDisappear(animated)
+      print("5 ViewController.viewWillDisappear()")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+      super.viewDidDisappear(animated)
+      print("6 ViewController.viewDidDisappear()")
+    }
+    
+    func setupUI() {
+        print("07 SecondViewController.setupUI()")
+        self.view.backgroundColor = .yellow
+        
+        let label = UILabel()
+        label.text = "Second View"
+        label.textAlignment = .center
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.frame = CGRect(x: 20, y: 100, width: view.frame.width - 40, height: 40)
+        self.view.addSubview(label)
+    }
+
+}
--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -11,46 +11,31 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    print("1 ViewController.viewDidLoad()")
-
     setupUI()
   }
 
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    print("2 ViewController.viewWillAppear()")
-  }
-
-  override func viewIsAppearing(_ animated: Bool) {
-    super.viewIsAppearing(animated)
-    print("3 ViewController.viewIsAppearing()")
-  }
-
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    print("4 ViewController.viewDidAppear()")
-  }
-
-  override func viewWillDisappear(_ animated: Bool) {
-    super.viewWillDisappear(animated)
-    print("5 ViewController.viewWillDisappear()")
-  }
-
-  override func viewDidDisappear(_ animated: Bool) {
-    super.viewDidDisappear(animated)
-    print("6 ViewController.viewDidDisappear()")
-  }
-
   func setupUI() {
-    print("7 ViewController.setupUI()")
     let label = UILabel()
     label.text = "Hello, World!"
+    // (content layout) 라벨의 텍스트를 가운데 정렬
     label.textAlignment = .center
     label.textColor = .black
     label.font = UIFont.systemFont(ofSize: 24)
-    // frame layout
+    // (frame layout) 붙여질 뷰의 위치와 크기를 설정
     label.frame = CGRect(x: 20, y: 100, width: view.frame.width - 40, height: 40)
+    // self.view(메인 뷰)에 라벨을 서브 뷰로 추가
     view.addSubview(label)
+      
+      let button = UIButton()
+      button.setTitle("Go second", for: .normal)
+      button.setTitleColor(.blue, for: .normal)
+      button.frame = CGRect(x: 20, y: 200, width: view.frame.width - 40, height: 40)
+      button.addTarget(self, action: #selector(goSecond), for: .touchUpInside)
+      view.addSubview(button)
   }
+    
+    @objc func goSecond() {
+        let secondVC = SecondViewController()
+        self.present(secondVC, animated: true)
+    }
 }
