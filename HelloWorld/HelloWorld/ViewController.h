d021e473fcb0e4546d0da323d60c07c16150b9f9
kdy <kokbael@naver.com>
2025-03-12 11:30:46 +0900

    UIKit UIViewController 실습 첫 프로젝트 생성

---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by 김동영 on 3/12/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by 김동영 on 3/12/25.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
