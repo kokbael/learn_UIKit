3c6cc19a809ed8a5cfb11113333d14d9a3eff096
kdy <kokbael@naver.com>
2025-03-13 14:52:44 +0900

    폼 테이블 뷰 컨트롤러 개선: UISwitch 및 UIButton 추가, 섹션 수 및 헤더/푸터 설정 변경

--- UIViewDemo/UIViewDemo/FormTableViewController.swift
+++ UIViewDemo/UIViewDemo/FormTableViewController.swift
@@ -8,59 +8,77 @@
 import UIKit
 
 class FormTableViewController: UITableViewController {
-
+    var flag = false
+    var toggle = UISwitch()
+    let button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        self.tableView.separatorColor = .black
+        
+        toggle.addTarget(self, action: #selector(toggleChanged), for: .valueChanged)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = .systemBlue
+        config.title = "Button"
+        button.isEnabled = flag
+        button.configuration = config
+        button.sizeToFit()
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 4
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        switch section {
-        case 0:
-            return 1
-        case 3:
-            return 3
-        default:
-            return 2
-        }    }
-
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section Header"
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return "Section Footer"
+    }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        
+        if indexPath.row == 0 {
+            toggle.isOn = flag
+            cell.accessoryView = toggle
+        } else {
+            cell.accessoryView = button
+        }
+        
         
         // Configure the cell...
         var config = UIListContentConfiguration.subtitleCell()
         config.text = "Section: \(indexPath.section), Row: \(indexPath.row)"
         cell.contentConfiguration = config
-
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        switch indexPath.section {
-        case 0:
-            return 100
-        case 1:
-            return 200
-        default:
-            return 50
-        }
+        return 50
     }
     
+    // MARK: - Table view data source
+    @objc func toggleChanged(_ sender: UISwitch) {
+        flag = sender.isOn
+        button.isEnabled = flag
+    }
+    
+    @objc func buttonTapped(_ sender: UIButton) {
+        print("Button tapped")
+    }
 }
 
 #Preview {
