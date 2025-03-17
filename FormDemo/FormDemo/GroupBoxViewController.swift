f124ea4ecd1c5d336cdcbccec76b37dd80298db9
kdy <kokbael@naver.com>
2025-03-17 09:30:30 +0900

    그룹 박스 뷰 추가

--- FormDemo/FormDemo/GroupBoxViewController.swift
+++ FormDemo/FormDemo/GroupBoxViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class GroupBoxViewController: UIViewController {
+    
+    var flag = false
+    let groupBox = UIView()
+    let groupBoxLabel = UILabel()
+    let toggle = UISwitch()
+    let textField = UITextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.backgroundColor = .white
+        
+        setupGroupBox()
+    }
+    
+    func setupGroupBox() {
+        groupBox.layer.borderWidth = 1
+        groupBox.layer.borderColor = UIColor.lightGray.cgColor
+        groupBox.layer.cornerRadius = 8
+        groupBox.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(groupBox)
+        
+        NSLayoutConstraint.activate([
+            groupBox.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            groupBox.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            groupBox.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            groupBox.heightAnchor.constraint(equalToConstant: 200),
+        ])
+        
     }
 
 
