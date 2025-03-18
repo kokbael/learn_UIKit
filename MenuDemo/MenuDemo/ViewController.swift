c4c452bf70a7e458548572da9bb12a0298052dfe
kdy <kokbael@naver.com>
2025-03-18 10:02:49 +0900

    MenuDemo 프로젝트 생성

---
+++ MenuDemo/MenuDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenuDemo
+//
+//  Created by 김동영 on 3/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
