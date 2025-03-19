3721beddafb69457bd92906f83dfb1b13886de23
kdy <kokbael@naver.com>
2025-03-19 13:18:17 +0900

    탭 바 컨트롤러 및 두 개의 뷰 컨트롤러 추가, 기존 ViewController 삭제

--- TabDemo/TabDemo/TabBarController.swift
+++ TabDemo/TabDemo/TabBarController.swift
@@ -8,25 +8,38 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tabBar.tintColor = .systemGreen
-        // Do any additional setup after loading the view.
+        
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = .systemBackground
+        
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+        
+        setupTabBar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupTabBar() {
+        // 뷰 컨트롤러 생성
+        let firstVC = FirstViewController()
+        let secondVC = SecondViewController()
+        
+        // 네비게이션 컨트롤러 생성
+        let firstNavController = UINavigationController(rootViewController: firstVC)
+        let secondNavController = UINavigationController(rootViewController: secondVC)
+        
+        // 탭바 뷰 컨트롤러 배열 설정
+        self.viewControllers = [firstNavController, secondNavController]
+        
+        // 탭바 아이템 설정
+        firstNavController.tabBarItem = UITabBarItem(title: "첫번째", image: UIImage(systemName: "1.circle"),tag:  0)
+        secondNavController.tabBarItem = UITabBarItem(title: "두번째", image: UIImage(systemName: "2.circle"),tag:  1)
+        
     }
-    */
-
 }
+
 #Preview {
-   UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()!
- }
+    UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()!
+}
