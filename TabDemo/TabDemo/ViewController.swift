424692907c39a776880502586bda329b53c137fd
kdy <kokbael@naver.com>
2025-03-19 11:46:30 +0900

    탭 데모 프로젝트 생성

---
+++ TabDemo/TabDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabDemo
+//
+//  Created by 김동영 on 3/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
