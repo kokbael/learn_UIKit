42cde87fd334e2f8eea2be2f141bad3bd879c58f
kdy <kokbael@naver.com>
2025-03-19 10:21:59 +0900

    네비게이션 바에 여러 바버튼 추가

--- NavigationDemo/NavigationDemo/SecondViewController.swift
+++ NavigationDemo/NavigationDemo/SecondViewController.swift
@@ -12,6 +12,7 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "두 번째 뷰"
+        view.backgroundColor = .yellow
         
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
@@ -21,7 +22,47 @@ class SecondViewController: UIViewController {
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
         navigationController?.navigationBar.tintColor = .white // 아이템 색상
+        
+        let leftButton = UIBarButtonItem(systemItem: .cancel, primaryAction: UIAction { [weak self] _ in
+            self?.leftButtonTapped()
+        })
+        navigationItem.leftBarButtonItem = leftButton
+        
+        let rightButton = UIBarButtonItem(
+            title: "완료",
+            primaryAction: UIAction { [weak self] _ in
+                self?.rightButtonTapped()
+            })
+        let editButton = UIBarButtonItem(
+            systemItem: .edit,
+            primaryAction: UIAction { [weak self] _ in
+                self?.editButtonTapped()
+            })
+        let searchButton = UIBarButtonItem(
+            systemItem: .search,
+            primaryAction: UIAction { [weak self] _ in
+                self?.searchButtonTapped()
+            })
+        
+        navigationItem.rightBarButtonItems = [rightButton, editButton, searchButton]
     }
+    
+    func leftButtonTapped() {
+        print("왼쪽 버튼 클릭")
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func rightButtonTapped() {
+       print("오른쪽 버튼 클릭")
+     }
+   
+     func editButtonTapped() {
+       print("편집 버튼 클릭")
+     }
+   
+     func searchButtonTapped() {
+       print("검색 버튼 클릭")
+     }
     
 }
 
