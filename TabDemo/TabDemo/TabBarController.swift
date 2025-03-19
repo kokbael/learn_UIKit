943b2e3c5410fcc20786444beb0f092cb753990f
kdy <kokbael@naver.com>
2025-03-19 11:46:38 +0900

    탭 바 컨트롤러 추가 및 스토리보드 시작 컨트롤러를 탭바 컨트롤러로 변경

--- TabDemo/TabDemo/TabBarController.swift
+++ TabDemo/TabDemo/TabBarController.swift
@@ -11,7 +11,7 @@ class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tabBar.tintColor = .systemGreen
         // Do any additional setup after loading the view.
     }
     
@@ -27,3 +27,6 @@ class TabBarController: UITabBarController {
     */
 
 }
+#Preview {
+   UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()!
+ }
