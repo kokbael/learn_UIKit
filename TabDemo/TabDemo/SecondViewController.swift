48978ef199e1198e0c2998d8a73293f7b42aae2c
kdy <kokbael@naver.com>
2025-03-19 14:23:17 +0900

    데이터 관리 기능 추가 및 두 개의 뷰 컨트롤러에 데이터 전달 구현

--- TabDemo/TabDemo/SecondViewController.swift
+++ TabDemo/TabDemo/SecondViewController.swift
@@ -8,22 +8,51 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    let dataLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20)
+        label.textAlignment = .center
+        label.text = "데이터가 없습니다."
+        return label
+    }()
+    private var observer: NSObjectProtocol? // 옵저버 참조 저장
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Second"
-        // Do any additional setup after loading the view.
+        
+        setupLabel()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupLabel() {
+        view.addSubview(dataLabel)
+        dataLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            dataLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            dataLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        observer = NotificationCenter.default.addObserver(forName: DataManager.dataChangedNotification, object: nil, queue: .main) { [weak self] _ in
+            self?.updateLabel()
+        }
     }
-    */
-
+    
+    
+    
+    func updateLabel() {
+        if DataManager.shared.data.isEmpty {
+            
+        } else {
+            dataLabel.text = DataManager.shared.data
+        }
+    }
+    
+    deinit {
+        if let observer = observer {
+            NotificationCenter.default.removeObserver(observer)
+        }
+    }
+    
 }
+
