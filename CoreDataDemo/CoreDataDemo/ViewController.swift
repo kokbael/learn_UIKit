4e4f3eda4855ba29c5ce43aa9853af94ebef5715
kdy <kokbael@naver.com>
2025-03-20 11:37:18 +0900

    CoreDataDemo 프로젝트 생성

---
+++ CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by 김동영 on 3/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
