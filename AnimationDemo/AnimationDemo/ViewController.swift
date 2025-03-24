61f391bcc2cc9e70c49784868ee7bb621d9c9f25
kdy <kokbael@naver.com>
2025-03-24 14:25:25 +0900

    애니메이션 뷰 및 버튼 구성 추가, 애니메이션 시작 기능 구현

--- AnimationDemo/AnimationDemo/ViewController.swift
+++ AnimationDemo/AnimationDemo/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let animationView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureAnimationView()
+        configureButton()
     }
-
-
+    
+    func configureAnimationView() {
+        animationView.frame = CGRect(x: 50, y: 100, width: 100, height: 100)
+        animationView.backgroundColor = .systemRed
+        animationView.layer.cornerRadius = 8
+        view.addSubview(animationView)
+    }
+    
+    func configureButton() {
+        let button = UIButton(type: .system)
+        button.setTitle("Start Animation", for: .normal)
+        
+        button.addAction(UIAction(handler: { [weak self] _ in
+            self?.startAnimation()
+        }), for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: animationView.bottomAnchor, constant: 20)
+        ])
+    }
+    
+    func startAnimation() {
+        UIView.animate(withDuration: 1.0) {
+            self.animationView.frame.origin.x += 100
+            self.animationView.frame.origin.y += 50
+        }
+    }
 }
 
+#Preview {
+    ViewController()
+}
